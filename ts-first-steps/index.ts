0564db1ac887b4f85b3352c42b7993923ea65b96
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-05-05 17:49:40 -0300

    calculator endpoint

--- ts-first-steps/index.ts
+++ ts-first-steps/index.ts
@@ -1,8 +1,24 @@
 import express from 'express';
+import { calculator, Operation } from './calculator';
+
+
 const app = express();
+app.use(express.json());
 
 app.get('/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { value1, value2, op } = req.body;
+
+  if ( !value1 || !value2 || !op || isNaN(Number(value1)) || isNaN(Number(value2)) ) {
+    return res.status(400).send({ error: 'malformatted parameters' });
+  }
+
+  const result = calculator(Number(value1), Number(value2), op as Operation);
+  return res.send({ result });
 });
 
 const PORT = 3003;
