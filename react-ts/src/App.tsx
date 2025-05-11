64539e6b31a1365705c084e32d3a64d27a49639a
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-05-11 16:25:53 -0300

    introduction to react + typescript

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+interface WelcomeProps {
+  name: string;
+}
+
+const Welcome = (props: WelcomeProps) => {
+  return <h1>Hello, {props.name}</h1>;
+};
+
+export default Welcome
---
+++ react-ts/src/main.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Welcome from './App.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <Welcome name="Sarah" />
+)
