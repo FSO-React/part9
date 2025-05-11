60920c050d3d28c2a7d1d3eb1a87c605f74c2676
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-05-10 23:26:59 -0300

    exercise 9.12 and 9.13

--- patientor-ts-backend/src/routes/patients.route.ts
+++ patientor-ts-backend/src/routes/patients.route.ts
@@ -1,11 +1,35 @@
 import express, { Response } from 'express';
 import patientsService from '../services/patients.service';
 import { NonSensitivePatient } from '../types';
+import { toNewPatient } from '../utils';
 
 const router = express.Router();
 
 router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
   res.send(patientsService.getNonSensitivePatients());
+});
+
+router.get('/:id', (req, res) => {
+  const patient = patientsService.findById(req.params.id);
+  if (patient) {
+    res.send(patient);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/', (req, res) => {
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedPatient = patientsService.addPatient(newPatient);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
--- patientor-ts-backend/src/services/patients.service.ts
+++ patientor-ts-backend/src/services/patients.service.ts
@@ -1,9 +1,14 @@
 import patients from '../../data/patients';
+import { v1 as uuid } from 'uuid';
 
-import { NonSensitivePatient, Patient } from '../types';
+import { NonSensitivePatient, Patient, NewPatient } from '../types';
 
 const getPatients = (): Patient[] => {
   return patients;
+};
+
+const findById = (id: string): Patient | undefined => {
+  return patients.find(p => p.id === id);
 };
 
 const getNonSensitivePatients = (): NonSensitivePatient[] => {
@@ -16,7 +21,18 @@ const getNonSensitivePatients = (): NonSensitivePatient[] => {
   }));
 };
 
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient : Patient = {
+    id: uuid(),
+    ...patient
+  };
+  patients.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getPatients,
+  findById,
   getNonSensitivePatients,
+  addPatient,
 };
