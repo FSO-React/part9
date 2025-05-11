94bbb872614ff857c825b89ef09600793eee5114
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-05-10 22:03:50 -0300

    validations and parsing

--- flight-diaries/src/routes/diaries.ts
+++ flight-diaries/src/routes/diaries.ts
@@ -1,7 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express, { Response } from 'express';
 import diaryService from '../services/diary.service';
 import { NonSensitiveDiaryEntry } from '../types';
+import toNewDiaryEntry from '../utils';
+
 
 const router = express.Router();
 
@@ -20,14 +22,17 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { date, weather, visibility, comment } = req.body;
-  const addedEntry = diaryService.addDiary({
-    date,
-    weather,
-    visibility,
-    comment,
-  });
-  res.json(addedEntry);
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
