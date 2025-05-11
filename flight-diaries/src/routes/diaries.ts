c09104455ed41da788990c67600d70ce5cbebeb8
Alejandro Diaz Crivelli <alejandrodiazcrivelli@gmail.com>
2025-05-10 23:44:53 -0300

    validations with Zod for flight diaries

--- flight-diaries/src/routes/diaries.ts
+++ flight-diaries/src/routes/diaries.ts
@@ -1,11 +1,28 @@
 // /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import express, { Response } from 'express';
+import express, { Request, NextFunction, Response } from 'express';
 import diaryService from '../services/diary.service';
-import { NonSensitiveDiaryEntry } from '../types';
-import toNewDiaryEntry from '../utils';
-
+import { NonSensitiveDiaryEntry, NewDiaryEntry, DiaryEntry } from '../types';
+import { NewEntrySchema } from '../utils';
+import z from 'zod';
 
 const router = express.Router();
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => { 
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => { 
+  try {
+    NewEntrySchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
 
 router.get('/', (_req, res: Response<NonSensitiveDiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries());
@@ -21,18 +38,11 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  try {
-    const newDiaryEntry = toNewDiaryEntry(req.body);
-    const addedEntry = diaryService.addDiary(newDiaryEntry);
-    res.json(addedEntry);
-  } catch (error) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
-  }
+router.post('/', newDiaryParser, (req: Request<unknown, unknown, NewDiaryEntry>, res: Response<DiaryEntry>) => {  
+  const addedEntry = diaryService.addDiary(req.body);
+  res.json(addedEntry);
 });
+
+router.use(errorMiddleware);
 
 export default router;
